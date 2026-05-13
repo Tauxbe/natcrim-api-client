1aafe441b511829e255ee3490c5edd9bbf11e0dd
GitHub Action <action@github.com>
2026-05-13 05:44:13 +0000

    Auto-generated API clients for NatCrim API - 20260513-054413

--- clients/java/src/main/java/com/natcrim/api/client/Configuration.java
+++ clients/java/src/main/java/com/natcrim/api/client/Configuration.java
@@ -13,7 +13,7 @@
 
 package com.natcrim.api.client;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-05-12T05:33:45.234562Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-05-13T05:43:25.159090Z[Etc/UTC]")
 public class Configuration {
     public static final String VERSION = "1.0.0";
 
--- clients/java/src/main/java/com/natcrim/api/client/StringUtil.java
+++ clients/java/src/main/java/com/natcrim/api/client/StringUtil.java
@@ -16,7 +16,7 @@ package com.natcrim.api.client;
 import java.util.Collection;
 import java.util.Iterator;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-05-12T05:33:45.234562Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-05-13T05:43:25.159090Z[Etc/UTC]")
 public class StringUtil {
   /**
    * Check if the given array contains the given value (with case-insensitive comparison).
