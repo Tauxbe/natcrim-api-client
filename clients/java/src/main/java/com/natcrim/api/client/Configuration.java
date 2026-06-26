70037b09fe3f981e75f990fa163fbdefa85c2eed
GitHub Action <action@github.com>
2026-06-26 06:05:53 +0000

    Auto-generated API clients for NatCrim API - 20260626-060553

--- clients/java/src/main/java/com/natcrim/api/client/Configuration.java
+++ clients/java/src/main/java/com/natcrim/api/client/Configuration.java
@@ -13,7 +13,7 @@
 
 package com.natcrim.api.client;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-06-25T06:00:52.053103Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-06-26T06:04:55.557423Z[Etc/UTC]")
 public class Configuration {
     public static final String VERSION = "1.0.0";
 
--- clients/java/src/main/java/com/natcrim/api/client/auth/HttpBearerAuth.java
+++ clients/java/src/main/java/com/natcrim/api/client/auth/HttpBearerAuth.java
@@ -20,7 +20,7 @@ import java.net.URI;
 import java.util.Map;
 import java.util.List;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-06-25T06:00:52.053103Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-06-26T06:04:55.557423Z[Etc/UTC]")
 public class HttpBearerAuth implements Authentication {
   private final String scheme;
   private String bearerToken;
