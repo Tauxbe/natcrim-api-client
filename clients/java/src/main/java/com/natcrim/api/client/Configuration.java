939526d56dec1fb042d680e0cacde55a9734cca4
GitHub Action <action@github.com>
2026-07-28 05:00:27 +0000

    Auto-generated API clients for NatCrim API - 20260728-050027

--- clients/java/src/main/java/com/natcrim/api/client/Configuration.java
+++ clients/java/src/main/java/com/natcrim/api/client/Configuration.java
@@ -13,7 +13,7 @@
 
 package com.natcrim.api.client;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-07-27T05:43:31.853136Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-07-28T04:59:26.195728Z[Etc/UTC]")
 public class Configuration {
     public static final String VERSION = "1.0.0";
 
--- clients/java/src/main/java/org/openapitools/client/model/HTTPValidationError.java
+++ clients/java/src/main/java/org/openapitools/client/model/HTTPValidationError.java
@@ -52,7 +52,7 @@ import com.natcrim.api.client.JSON;
 /**
  * HTTPValidationError
  */
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-07-27T05:43:31.853136Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-07-28T04:59:26.195728Z[Etc/UTC]")
 public class HTTPValidationError {
   public static final String SERIALIZED_NAME_DETAIL = "detail";
   @SerializedName(SERIALIZED_NAME_DETAIL)
